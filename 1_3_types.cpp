d006fb963be7b6efa5b8e4a4d4c6625a2ee2be1b
Timur Gafarov <timu.ryan@yandex.ru>
2024-05-09 17:18:51 +0300

    int8_t types

---
+++ 1_3_types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  int8_t x = 8;
+  uint8_t ux = 8;
+
+  
+  int16_t x16 = 16;
+  int32_t x32 = 32;
+  int64_t x64 = 64;
+  // int128_t x128 = 128;
+
+  // std::cout << x;
+}
+
