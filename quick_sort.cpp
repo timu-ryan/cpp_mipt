dc11cce279263e5d425f2539a4f440301f98f632
Timur Gafarov <timu.ryan@yandex.ru>
2024-05-16 03:06:04 +0300

    first quick sort realization

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
+// using namespace std;
 
-// quick sort
+void swap(int arr[] , int pos1, int pos2) {
+	int temp;
+	temp = arr[pos1];
+	arr[pos1] = arr[pos2];
+	arr[pos2] = temp;
+}
+
+int partition(int arr[], int low, int high, int pivot) {
+	int i = low;
+	int j = low;
+	while( i <= high){
+		if(arr[i] > pivot){
+			i++;
+		}
+		else{
+			swap(arr,i,j);
+			i++;
+			j++;
+		}
+	}
+	return j-1;
+}
+
+void quickSort(int arr[], int low, int high) {
+	if(low < high) {
+	int pivot = arr[high];
+	int pos = partition(arr, low, high, pivot);
+	
+	quickSort(arr, low, pos-1);
+	quickSort(arr, pos+1, high);
+	}
+}
 
 int main() {
-
+	int n;
+  std::cout << " enter the size of array";
+  std::cin >> n;
+	int arr[n];
+	for(int i = 0; i < n; i++){
+    std::cin >> arr[i];
+	}
+	quickSort(arr, 0 , n-1);
+  std::cout << "The sorted array is: ";
+	for(int i = 0; i < n; i++){
+    std::cout << arr[i] << "\t";
+	}
+	
 }
 
